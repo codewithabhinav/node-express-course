17bad5412deae80652d2119392174b67d2d5aaf1
codewithabhinav <presonalabhi@gmail.com>
2021-06-30 13:11:23 +0530

    add dynamic route

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ app.get('/users', function(req,res){
               message: 'successfully got users. Nice!',
               users: mockUserData
          })
-    })
+})
+    
+app.get('/users/:id',function(req,res){
+	// console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
 
 
 
