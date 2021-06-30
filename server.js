6afcd2493dd184c1fe039604530dff89b9f33e86
codewithabhinav <presonalabhi@gmail.com>
2021-06-30 18:27:38 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+// const express = require('express');
+// // const app = express();
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json())
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json())
 
 
 const mockUserData=[
@@ -15,7 +22,13 @@ app.get('/users', function(req,res){
               users: mockUserData
          })
 })
-    
+//  TODO You can use dynamic variables to search for specific data associated with an id in your database,
+// and return that(instead of just returning the id).   
+
+    // In Express, words with a colon 
+// in front of them in the url are treated as variables.
+// You can access the value of each variable through req.params, like this:
+
 app.get('/users/:id',function(req,res){
 	// console.log(req.params.id)
 	res.json({
@@ -25,8 +38,26 @@ app.get('/users/:id',function(req,res){
 	})
 })
 
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
+})
 
 
-    app.listen(8000, function () {
-        console.log('server is Running');
-    })
+app.listen(8000, function () {console.log('server is Running');})
